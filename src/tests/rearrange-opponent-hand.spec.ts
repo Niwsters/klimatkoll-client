4077af555152ad289d95e75895c968c85434631f
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-03-13 14:41:39 +0100

    Refactor test

--- src/tests/rearrange-opponent-hand.spec.ts
+++ src/tests/rearrange-opponent-hand.spec.ts
@@ -1,6 +1,8 @@
 import { Card } from '../game/card'
 import { ANIMATION_DURATION_MS, HAND_CARD_ANGLE, HAND_X_RADIUS, HAND_Y_RADIUS, OPPONENT_HAND_POSITION } from '../game/constants'
 import { Factory } from './test-factory'
+import { spec } from './spec'
+import { GameState } from '../game/gamestate'
 
 function getOpponentHandCardPosition(i: number, cardCount: number): number[] {
   const n = cardCount - 1
@@ -11,9 +13,49 @@ function getOpponentHandCardPosition(i: number, cardCount: number): number[] {
   return [x, y]
 }
 
+function initCards() {
+  const card = new Card(0, "blargh", "opponent-hand")
+  const card2 = new Card(1, "1337", "opponent-hand")
+  return [card, card2]
+}
+
+function expectedCards() {
+  let [card, card2] = initCards()
+
+  let pos1 = getOpponentHandCardPosition(0, 2)
+  let pos2 = getOpponentHandCardPosition(1, 2)
+
+  card = Card.move(card, pos1[0], pos1[1], currentTime)
+  card = Card.rotateGlobal(card, 2.9845130209103035, currentTime)
+  card = Card.update(card, currentTime + ANIMATION_DURATION_MS)
+
+  card2 = Card.move(card2, pos2[0], pos2[1], currentTime)
+  card2 = Card.rotateGlobal(card2, 3.2986722862692828, currentTime)
+  card2 = Card.update(card2, currentTime + ANIMATION_DURATION_MS)
+
+  return [card, card2]
+}
+
+const currentTime = 1337
+
+// moves OpponentHand cards to their positions
+spec('Rearrange opponent hand')
+  .when(() => {
+    const state = Factory.GameState()
+    const [card, card2] = initCards()
+
+    state.opponentHand = state.opponentHand.addCard(card)
+    state.opponentHand = state.opponentHand.addCard(card2)
+
+    return state
+      .update(currentTime) // Queue animations
+      .update(currentTime + ANIMATION_DURATION_MS) // Finish animations
+  })
+  .expect((state: GameState) => state.cards)
+  .toEqual(expectedCards())
+
 describe('Rearrange opponent hand', () => {
   const state = Factory.GameState()
-  const currentTime = 1337
 
   it('moves OpponenHand cards to their positions', () => {
     let card = new Card(0, "blargh", "opponent-hand")
