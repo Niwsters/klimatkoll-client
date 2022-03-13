741e863fd8c419e3ea11c214929302eb37323c09
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-03-13 19:12:57 +0100

    Fully remove dependency on Jest's describe and expect

---
+++ src/tests/card-animation.spec.ts
@@ -0,0 +1,28 @@
+import { Card } from '../game/card'
+import { ANIMATION_DURATION_MS } from '../game/constants'
+import { spec } from './spec'
+
+const test = spec('Card animation')
+  .when(() => new Card(3, "blargh", "hand"))
+
+test
+  .when(card => card = Card.move(card, 1337, 1337, 0))
+  .when(card => Card.update(card, ANIMATION_DURATION_MS))
+  .expect(card => card.position).toEqual([1337, 1337])
+
+test
+  .when(card => card = Card.rotateGlobal(card, 1337, 0))
+  .when(card => Card.update(card, ANIMATION_DURATION_MS))
+  .expect(card => card.rotation).toEqual(1337)
+
+test
+  .when(card => Card.rotateLocal(card, 1337, 0))
+  .when(card => Card.update(card, ANIMATION_DURATION_MS))
+  .expect(card => card.addedRotation).toEqual(1337)
+
+test
+  .when(card => Card.scale(card, 1337, 0))
+  .when(card => Card.update(card, ANIMATION_DURATION_MS))
+  .expect(card => card.scale).toEqual(1337)
+
+describe('', () => it('', () => {}))
