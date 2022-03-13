4ed7bbe11d8bb97af8f103bc1f3d65dd8e38832c
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-03-13 15:07:58 +0100

    Refactor unit test

--- src/tests/hover-emissions-line.spec.ts
+++ src/tests/hover-emissions-line.spec.ts
@@ -3,74 +3,79 @@ import { Card } from "../game/card"
 import { ANIMATION_DURATION_MS, EMISSIONS_LINE_POSITION } from "../game/constants"
 import { GameState } from "../game/gamestate"
 import { Factory } from './test-factory'
+import { spec } from './spec'
 
-describe('Hovering EmissionsLine cards', () => {
-  const currentTime = 1337
-  const card = new Card(0, "some-card", "emissions-line")
+function moveMouse(state: GameState, x: number, y: number): GameState {
+  const event = {...new MouseMovedEvent(x, y), event_id: 0}
+  state = state.mouse_moved(event)[0]
+  state = state.update(currentTime + ANIMATION_DURATION_MS)
+  return state
+}
 
-  let state: GameState
-  beforeEach(() => {
-    state = Factory.GameState()
+function getCardScale(state: GameState, cardID: number) {
+  return state.emissionsLine.cards.find(c => c.id === cardID).scaleGoal.scale
+}
+
+function getOtherCards(state: GameState) {
+  return state.emissionsLine.cards.filter(c => c.id !== card.id)
+}
+
+function unique(array: any[]) {
+  return [...new Set(array)]
+}
+
+const currentTime = 1337
+const card = new Card(0, "some-card", "emissions-line")
+
+const test = spec('Hovering EmissionsLine cards')
+  .when(() => {
+    const state = Factory.GameState()
     state.emissionsLine = state.emissionsLine.addCard(card, 0, currentTime)
-  })
-
-  function moveMouse(state: GameState, x: number, y: number): GameState {
-    const event = {...new MouseMovedEvent(x, y), event_id: 0}
-    state = state.mouse_moved(event)[0]
-    state = state.update(currentTime + ANIMATION_DURATION_MS)
     return state
-  }
-
-  function getCardScale(state: GameState, cardID: number) {
-    return state.emissionsLine.cards.find(c => c.id === cardID).scaleGoal.scale
-  }
-
-  it("zooms in on emissions line card", () => {
-    const [x, y] = EMISSIONS_LINE_POSITION
-    state = moveMouse(state, x, y)
-
-    // Zooms in on middle card
-    expect(getCardScale(state, card.id)).toEqual(Card.DEFAULT_SCALE * 2)
   })
 
-  it("doesn't zoom in on surrounding cards", () => {
+const hovering = test
+  .when((state: GameState) => {
     const [x, y] = EMISSIONS_LINE_POSITION
-    state = moveMouse(state, x, y)
-
-    const otherCards = state.emissionsLine.cards.filter(c => c.id !== card.id)
-    for (const card of otherCards) {
-      expect(getCardScale(state, card.id)).toEqual(Card.DEFAULT_SCALE)
-    }
-
-    expect(getCardScale(state, card.id)).toEqual(Card.DEFAULT_SCALE * 2)
+    return moveMouse(state, x, y)
   })
 
-  it("zooms out if mouse moves outside emissions line Y bounds", () => {
-    const [x, y] = EMISSIONS_LINE_POSITION
-    state = moveMouse(state, x, y)
+// zooms in on emissions line card
+hovering
+  .expect((state: GameState) => getCardScale(state, card.id))
+  .toEqual(Card.DEFAULT_SCALE * 2)
 
-    // Move mouse outside emissions line
-    state = moveMouse(state, 0, 0)
+// doesn't zoom in on surrounding cards
+hovering
+  .expect((state: GameState) => unique(getOtherCards(state).map(c => c.scaleGoal.scale)))
+  .toEqual([Card.DEFAULT_SCALE])
 
-    expect(getCardScale(state, card.id)).toEqual(Card.DEFAULT_SCALE)
-  })
+// zooms out if mouse moves outside emissions line Y bounds
+hovering
+  .when((state: GameState) => moveMouse(state, 0, 0))
+  .expect((state: GameState) => getCardScale(state, card.id))
+  .toEqual(Card.DEFAULT_SCALE)
 
-  it("zooms in only on the closest card to the mouse", () => {
-    const card2 = new Card(1, "other-card", "emissions-line")
+// zooms in only on the closest card to the mouse
+const card2 = new Card(1, "other-card", "emissions-line")
+const twoCards = test
+  .when((state: GameState) => {
     card2.position = EMISSIONS_LINE_POSITION
     state.emissionsLine = state.emissionsLine.addCard(card2, 2, currentTime)
 
     const [x, y] = [card2.position[0], EMISSIONS_LINE_POSITION[1]]
-    state = moveMouse(state, x, y)
-
-    expect(getCardScale(state, card.id)).toEqual(Card.DEFAULT_SCALE * 2)
-    expect(getCardScale(state, card2.id)).toEqual(Card.DEFAULT_SCALE)
+    return moveMouse(state, x, y)
   })
+twoCards.expect((state: GameState) => getCardScale(state, card.id)).toEqual(Card.DEFAULT_SCALE * 2)
+twoCards.expect((state: GameState) => getCardScale(state, card2.id)).toEqual(Card.DEFAULT_SCALE)
 
-  it("zooms in only if mouse is within emissions line x-axis", () => {
+// zooms in only if mouse is within emissions line x-axis
+test
+  .when((state: GameState) => {
     const y = EMISSIONS_LINE_POSITION[1]
-    state = moveMouse(state, 0, y)
-
-    expect(getCardScale(state, card.id)).toEqual(Card.DEFAULT_SCALE)
+    return moveMouse(state, 0, y)
   })
-})
+  .expect((state: GameState) => getCardScale(state, card.id))
+  .toEqual(Card.DEFAULT_SCALE)
+
+describe('', () => it('', () => {}))
