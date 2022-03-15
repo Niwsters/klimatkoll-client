0b220f3407a6b93ff1813212dca9f670b5597f0a
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-03-15 19:06:15 +0100

    Refactor Card tests

--- src/tests/card-animation.spec.ts
+++ src/tests/card-animation.spec.ts
@@ -1,27 +1,31 @@
-import { Card, ICard } from '../game/card'
+import { Card } from '../game/card'
 import { ANIMATION_DURATION_MS } from '../game/constants'
 import { spec } from './spec'
+
+function finishAnimation(card: Card): Card {
+  return card.update(ANIMATION_DURATION_MS)
+}
 
 export default function main() {
   const test = spec().when(() => new Card(3, "blargh", "hand"))
 
   test
-    .when((card: Card) => card = card.move(1337, 1337, 0))
-    .when((card: Card) => Card.update(card, ANIMATION_DURATION_MS))
-    .expect((card: ICard) => card.position).toEqual([1337, 1337])
+    .when((card: Card) => card.move(1337, 1337, 0))
+    .when(finishAnimation)
+    .expect((card: Card) => card.position).toEqual([1337, 1337])
 
   test
-    .when(card => card = Card.rotateGlobal(card, 1337, 0))
-    .when(card => Card.update(card, ANIMATION_DURATION_MS))
+    .when((card: Card) => card.rotateGlobal(1337, 0))
+    .when(finishAnimation)
     .expect(card => card.rotation).toEqual(1337)
 
   test
-    .when(card => Card.rotateLocal(card, 1337, 0))
-    .when(card => Card.update(card, ANIMATION_DURATION_MS))
+    .when(card => card.rotateLocal(1337, 0))
+    .when(finishAnimation)
     .expect(card => card.addedRotation).toEqual(1337)
 
   test
-    .when(card => Card.scale(card, 1337, 0))
-    .when(card => Card.update(card, ANIMATION_DURATION_MS))
+    .when(card => card.setScale(1337, 0))
+    .when(finishAnimation)
     .expect(card => card.scale).toEqual(1337)
 }
--- src/tests/rearrange-opponent-hand.spec.ts
+++ src/tests/rearrange-opponent-hand.spec.ts
@@ -4,7 +4,7 @@ import { Factory } from './test-factory'
 import { spec } from './spec'
 import { GameState } from '../game/gamestate'
 
-function getOpponentHandCardPosition(i: number, cardCount: number): number[] {
+function getOpponentHandCardPosition(i: number, cardCount: number): [number, number] {
   const n = cardCount - 1
   const angle = HAND_CARD_ANGLE * (i - n/2) + Math.PI
   const x = OPPONENT_HAND_POSITION[0] + HAND_X_RADIUS * Math.sin(angle)
@@ -13,25 +13,23 @@ function getOpponentHandCardPosition(i: number, cardCount: number): number[] {
   return [x, y]
 }
 
-function initCards(): [ICard, ICard] {
+function initCards(): [Card, Card] {
   const card = new Card(0, "blargh", "opponent-hand")
   const card2 = new Card(1, "1337", "opponent-hand")
   return [card, card2]
 }
 
+function moveCard(card: Card, position: [number, number], rotation: number): Card {
+  card = card.move(position[0], position[1], currentTime)
+  card = card.rotateGlobal(rotation, currentTime)
+  return card.update(currentTime + ANIMATION_DURATION_MS)
+}
+
 function expectedCards() {
   let [card, card2] = initCards()
 
-  let pos1 = getOpponentHandCardPosition(0, 2)
-  let pos2 = getOpponentHandCardPosition(1, 2)
-
-  card = Card.move(card, pos1[0], pos1[1], currentTime)
-  card = Card.rotateGlobal(card, 2.9845130209103035, currentTime)
-  card = Card.update(card, currentTime + ANIMATION_DURATION_MS)
-
-  card2 = Card.move(card2, pos2[0], pos2[1], currentTime)
-  card2 = Card.rotateGlobal(card2, 3.2986722862692828, currentTime)
-  card2 = Card.update(card2, currentTime + ANIMATION_DURATION_MS)
+  card = moveCard(card, getOpponentHandCardPosition(0, 2), 2.9845130209103035)
+  card2 = moveCard(card2, getOpponentHandCardPosition(1, 2), 3.2986722862692828)
 
   return [card, card2]
 }
