0ce586f26fd30c468af5552043098a1ec3010788
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-05-15 15:24:59 +0200

    Add singleplayer hand (event sourced, yay)

--- src/pages/singleplayer/singleplayer.tsx
+++ src/pages/singleplayer/singleplayer.tsx
@@ -1,23 +1,67 @@
-import { ICard } from "@shared/models";
+import { EventToAdd } from "@shared/events";
 import { Card } from "core/card";
-import { Deck } from "core/deck";
+import { Hand } from "core/hand";
 import { Page } from "pages/page";
 import { Services } from "pages/page-factory";
 import React from "react";
 import { fetchCardData } from "shared/fetch-card-data";
 
-async function getCards(baseUrl: string): Promise<ICard[]> {
+async function getCards(baseUrl: string): Promise<Card[]> {
   const cardData = await fetchCardData(baseUrl)
 
   return cardData.map((c, i) => new Card(i, c.name))
 }
 
+function draw_card(hand: Hand, event: any) {
+  return hand.addCard(event.payload.card).update(event.timestamp, 0, 0)
+}
+
+type HandEventHandler = (hand: Hand, event: any) => Hand
+
+type HandEventHandlers = {
+  [event_type: string]: HandEventHandler
+}
+
+const eventHandlers: HandEventHandlers = {
+  draw_card
+}
+
+function handleEvent(hand: Hand, event: any): Hand {
+  return eventHandlers[event.event_type](hand, event)
+}
+
+function getHand(events: any[]): Hand {
+  return events.reduce(handleEvent, new Hand())
+}
+
+function drawCard(cards: Card[]): any {
+  return {
+    event_type: "draw_card",
+    payload: { card: cards[0] },
+    timestamp: Date.now()
+  }
+}
+
+type EventStreams = { [stream: string]: EventToAdd[] }
+
 export class SinglePlayerPage implements Page {
   component: React.ReactElement = <h1>oh hi</h1>;
-  cards: ICard[] = [];
+
+  private events: EventStreams = {
+    'hand': []
+  }
 
   private async getCards(baseUrl: string) {
-    this.cards = await getCards(baseUrl)
+    const allCards = await getCards(baseUrl)
+    this.events['hand'] = [drawCard(allCards)]
+  }
+
+  get cards(): Card[] {
+    return [...this.hand.cards]
+  }
+
+  get hand(): Hand {
+    return getHand(this.events['hand']).update(Date.now(), 0, 0)
   }
 
   constructor(services: Services) {
