a592701b1481f3ee3a056553d3af544107404330
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-06-04 20:42:02 +0200

    Add game over message

--- src/pages/singleplayer/ui/status-info.tsx
+++ src/pages/singleplayer/ui/status-info.tsx
@@ -14,6 +14,41 @@ function correctCards(spState: SPState): number {
   return spState.emissionsLine.length - 1
 }
 
+function isGameOver(spState: SPState): boolean {
+  return cardsLeft(spState) === 0
+}
+
+function Info(props: Props): React.ReactElement {
+  return (
+    <div>
+      <div>
+        Cards left: { cardsLeft(props.spState) }
+      </div>
+      <br />
+      <div>
+        Correct placements: { correctCards(props.spState) }
+      </div>
+    </div>
+  )
+}
+
+function GameOver(props: Props): React.ReactElement {
+  return (
+    <div>
+      Game over! You placed { correctCards(props.spState) } cards correctly
+    </div>
+  )
+}
+
+function StatusMessage(props: Props): React.ReactElement {
+  switch (isGameOver(props.spState)) {
+    case true:
+      return <GameOver spState={props.spState} />
+    case false:
+      return <Info spState={props.spState} />
+  }
+}
+
 export function StatusInfo(props: Props): React.ReactElement {
   const style: Style = {
     "flex-grow": "1",
@@ -22,13 +57,7 @@ export function StatusInfo(props: Props): React.ReactElement {
 
   return (
     <div style={style}>
-      <div>
-        Cards left: { cardsLeft(props.spState) }
-      </div>
-      <br />
-      <div>
-        Correct placements: { correctCards(props.spState) }
-      </div>
+      <StatusMessage spState={props.spState} />
     </div>
   )
 }
